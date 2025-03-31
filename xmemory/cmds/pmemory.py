806cc62339159909f13329b567561b67d9cb94a0
Mingzhe Zou <zoumingzhe@qq.com>
2025-03-31 17:44:30 +0800

    pmemory: improve memory information display and remove unused attributes

--- xmemory/cmds/pmemory.py
+++ xmemory/cmds/pmemory.py
@@ -10,13 +10,11 @@ from xkits_command import Command
 from xkits_command import CommandArgument
 from xkits_command import CommandExecutor
 
-from xmemory.attribute import __description__
-from xmemory.attribute import __project__
 from xmemory.attribute import __urlhome__
 from xmemory.attribute import __version__
 
 
-@CommandArgument(__project__, description=__description__)
+@CommandArgument("pmemory", description="View memory usage of a process")
 def add_cmd(_arg: ArgParser):  # pylint: disable=unused-argument
     pass
 
@@ -24,12 +22,13 @@ def add_cmd(_arg: ArgParser):  # pylint: disable=unused-argument
 @CommandExecutor(add_cmd)
 def run_cmd(cmds: Command) -> int:  # pylint: disable=unused-argument
     pid: int = os.getpid()
-    cmds.stdout(f"Hello, PID {pid}")
     process: Process = Process(pid)
     memory_info = process.memory_info()
-    cmds.stdout(f"VMS (Virtual Memory Size)\t{memory_info.vms / 1024 / 1024:.2f} MB\t{memory_info.vms}")  # noqa:E501
-    cmds.stdout(f"RSS (Resident Set Size)  \t{memory_info.rss / 1024 / 1024:.2f} MB\t{memory_info.rss}")  # noqa:E501
-    cmds.stdout("Goodbye!")
+    cmds.stdout(f"PID {pid} memory usage:")
+    cmds.stdout("RSS (Resident Set Size)         VMS (Virtual Memory Size)")
+    rss: str = f"{memory_info.rss / 1048576:.02f} MB / {memory_info.rss}"
+    vms: str = f"{memory_info.vms / 1048576:.02f} MB / {memory_info.vms}"
+    cmds.stdout(f"{rss:<30}  {vms}")
     return 0
 
 
