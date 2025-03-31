d96f602ab353398957977fa9f49a1e417c9984fc
Mingzhe Zou <zoumingzhe@outlook.com>
2025-04-01 00:24:41 +0800

    bump version to 0.1

--- xmemory/attribute.py
+++ xmemory/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xmemory"
-__version__ = "0.1.alpha.1"
+__version__ = "0.1"
 __urlhome__ = "https://github.com/bondbox/xmemory/"
 __description__ = "Memory usage analysis"
 
